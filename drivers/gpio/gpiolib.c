aca92b20855203105f162fdeb20accd0a32d4322
Unknown <unterwelt1337@yahoo.de>
2018-04-15 11:59:28 +0200

    Revert "fix"
    This reverts commit cb9e7c0472ff1af99318d1badefb02cfcc704125.

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -63,20 +63,12 @@ static inline void desc_set_label(struct gpio_desc *d, const char *label)
 /**
  * Convert a GPIO number to its descriptor
  */
-#ifdef CONFIG_MACH_XIAOMI_MIDO
-static int special_irq;
-#endif
 struct gpio_desc *gpio_to_desc(unsigned gpio)
 {
-#ifdef CONFIG_MACH_XIAOMI_MIDO
-	if (gpio == 65)
-		special_irq = 1;
-	else
-		special_irq = 0;
-#endif
-	if (WARN(!gpio_is_valid(gpio), "invalid GPIO %d\n", gpio))
+	if (!gpio_is_valid(gpio)) {
+		pr_err("invalid GPIO %d\n", gpio);
 		return NULL;
-	else
+	} else
 		return &gpio_desc[gpio];
 }
 EXPORT_SYMBOL_GPL(gpio_to_desc);
@@ -992,9 +984,6 @@ int gpiod_direction_input(struct gpio_desc *desc)
 	return status;
 }
 EXPORT_SYMBOL_GPL(gpiod_direction_input);
-#ifdef CONFIG_MACH_XIAOMI_MIDO
-extern int gt9xx_flag;
-#endif
 
 static int _gpiod_direction_output_raw(struct gpio_desc *desc, int value)
 {
@@ -1002,25 +991,13 @@ static int _gpiod_direction_output_raw(struct gpio_desc *desc, int value)
 	int			status = -EINVAL;
 
 	/* GPIOs used for IRQs shall not be set as output */
-#ifdef CONFIG_MACH_XIAOMI_MIDO
-	if ((special_irq == 1) && (gt9xx_flag == 1)) {
-		printk("[GPIO]set GPIO_65 as irq output\n");
-	} else{
-		if (test_bit(FLAG_USED_AS_IRQ, &desc->flags)) {
-			gpiod_err(desc,
-					"%s: tried to set a GPIO tied to an IRQ as output\n",
-					__func__);
-			return -EIO;
-		}
-	}
-#else
 	if (test_bit(FLAG_USED_AS_IRQ, &desc->flags)) {
 		gpiod_err(desc,
 			  "%s: tried to set a GPIO tied to an IRQ as output\n",
 			  __func__);
 		return -EIO;
 	}
-#endif
+
 	/* Open drain pin should not be driven to 1 */
 	if (value && test_bit(FLAG_OPEN_DRAIN,  &desc->flags))
 		return gpiod_direction_input(desc);
@@ -1904,3 +1881,4 @@ static int __init gpiolib_debugfs_init(void)
 subsys_initcall(gpiolib_debugfs_init);
 
 #endif	/* DEBUG_FS */
+
