efbfa97fbc26a57197b8c28302b5535d3e449e97
Unknown <unterwelt1337@yahoo.de>
2018-07-22 09:01:55 +0200

    Revert "prima: CAF TAG 'LA.UM.6.6.r1-09200-89xx.0'"
    This reverts commit f0918d599d516b51b742af7f04ee9e4036f4bd58.

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_hostapd.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_hostapd.c
@@ -221,26 +221,16 @@ int hdd_hostapd_open (struct net_device *dev)
   --------------------------------------------------------------------------*/
 int __hdd_hostapd_stop (struct net_device *dev)
 {
-   hdd_adapter_t *adapter = WLAN_HDD_GET_PRIV_PTR(dev);
-   hdd_context_t *hdd_ctx = WLAN_HDD_GET_CTX(adapter);
-   int ret;
-
    ENTER();
 
-   ret = wlan_hdd_validate_context(hdd_ctx);
-   if (0 != ret)
-       return ret;
+   if(NULL != dev) {
+       hddLog(VOS_TRACE_LEVEL_INFO, FL("Disabling queues"));
+       //Stop all tx queues
+       netif_tx_disable(dev);
 
-   hddLog(VOS_TRACE_LEVEL_INFO, FL("Disabling queues"));
-
-   //Stop all tx queues
-   netif_tx_disable(dev);
-
-   //Turn OFF carrier state
-   netif_carrier_off(dev);
-
-   if (!hdd_is_cli_iface_up(hdd_ctx))
-       sme_ScanFlushResult(hdd_ctx->hHal, 0);
+       //Turn OFF carrier state
+       netif_carrier_off(dev);
+   }
 
    EXIT();
    return 0;
